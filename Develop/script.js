271eb164233acc7d1de4bf9953188423e6c215e3
Jason South <jason.south@me.com>
2022-05-16 17:07:46 -0500

    add comments, varibles, for loop

--- Develop/script.js
+++ Develop/script.js
@@ -3,45 +3,83 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 
-
-function generatePassword() {
-  var length = prompt("Choose how many characters from 8 to 128");
-  while (length > 8 || length > 128 || isNaN(length)) {length = prompt("Please ender a number between 8-128");}
-
-  var confirmLower = confirm("Do you want you password to include lower case letters?");
-  var confirmUpper = confirm("Do you want you password to include upper case letters?");
-  var confirmNumbers = confirm("Do you want your password to include numbers?");
-  var confirmSpecial = confirm("Do you want your password to include special characters?");
-
-  var lower = arrayFromLowToHigh(97, 122);
-  var upper = arrayFromLowToHigh(65, 90);
-  var numbers = arrayFromLowToHigh(48, 57);
-  var symbols = arrayFromLowToHigh(33, 47)
-    .concat(arrayFromLowToHigh(58, 64))
-    .concat(arrayFromLowToHigh(91, 96))
-    .concat(arrayFromLowToHigh(123, 126));
-
-  function arrayFromLowToHigh(low, high){
-    var array = [];
-
-    for (var i = low; i <= high; i++){
-      array.push(i);
-      }
-    return array;
-  }
-  console.log(lower)
-  console.log(upper)
-  console.log(numbers)
-  console.log(symbols)
-}
-
-
+// messages displayed when generate password clicked
 function writePassword() {
-console.log("button was clicked")
-
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
+
+// generate password, which links variable password to passwordText.value
+function generatePassword() {
+  var length = prompt("Choose from 8 to 128 characters you want your password to contain");
+
+  while (length < 8 || length > 128 || isNaN(length)) {
+    length = prompt("Please ender a number between 8 to 128");
+  }
+  console.log(length);
+
+  var confirmLower = confirm("Do you want your password to include lower case letters?");
+  console.log("lowercase= " + confirmLower);
+  
+  var confirmUpper = confirm("Do you want your password to include upper case letters?");
+  console.log("uppercase= " + confirmUpper);
+  
+  var confirmNumbers = confirm("Do you want your password to include numbers?");
+  console.log("numbers= " + confirmNumbers);
+
+  var confirmSpecial = confirm("Do you want your password to include special characters?");
+  console.log("special characters= " + confirmSpecial);
+
+  // ASCII character codes
+  var lower = arrayFromLowToHigh(97, 122);
+  console.log(lower);
+
+  var upper = arrayFromLowToHigh(65, 90);
+  console.log(upper);
+
+  var numbers = arrayFromLowToHigh(48, 57);
+  console.log(numbers);
+
+  var symbols = arrayFromLowToHigh(33, 47)
+    .concat(arrayFromLowToHigh(58, 64))
+    .concat(arrayFromLowToHigh(91, 96))
+    .concat(arrayFromLowToHigh(123, 126));
+  console.log(symbols);
+
+  // generating a random list depending on selected characters
+  function arrayFromLowToHigh(low, high) {
+    var array = [];
+    for (var i = low; i <= high; i++) {
+      array.push(i);
+    }
+    return array;
+  }
+ 
+  // Pulls from character codes depending on characters selected from questions
+  var characters = [];
+  if (confirmLower) {
+    characters = characters.concat(lower);
+  }
+  if (confirmUpper) {
+    characters = characters.concat(upper);
+  }
+  if (confirmNumbers) {
+      characters = characters.concat(numbers);
+  }
+  if (confirmSpecial) {
+        characters = characters.concat(symbols);      
+  }
+  console.log(characters);
+  
+  var passwordCharacters = [];
+  for (var i = 0; i < length; i++) {
+    var characterCode = characters[Math.floor(Math.random() * characters.length)];
+    passwordCharacters.push(String.fromCharCode(characterCode));
+  console.log(passwordCharacters)
+  }
+  return passwordCharacters.join("");
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# javascript-password
+# Javascript-password
+
+## Description
+
+ A random secure password needs to be generated that meets the following criteria to provide greater security.
+ Give a series of prompts, which include the length of characters between 8 & 128, whether to include lowercase, uppercase, numeric, and/or symbols.  When the prompts are selected, a password should be generated that matches the selected criteria.  This password needs to be displayed or written to the page.  
+ In Javascript I created variables for messages and variables for arrary from low to high.  Then a function arrary fo
